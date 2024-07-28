3dcc06193cab27bc81e6a55d78f76e4803ed480b
ziyanx02 <ziyanx02@gmail.com>
2024-07-28 09:30:16 +0800

    fix typo

--- rsl_rl/rsl_rl/runners/on_policy_runner.py
+++ rsl_rl/rsl_rl/runners/on_policy_runner.py
@@ -107,7 +107,7 @@ class OnPolicyRunner:
             # Rollout
             with torch.inference_mode():
                 for i in range(self.num_steps_per_env):
-                    actions = self.alg.act(obs, critic_obs) * 0
+                    actions = self.alg.act(obs, critic_obs)
                     obs, privileged_obs, rewards, dones, infos = self.env.step(actions)
                     critic_obs = privileged_obs if privileged_obs is not None else obs
                     obs, critic_obs, rewards, dones = obs.to(self.device), critic_obs.to(self.device), rewards.to(self.device), dones.to(self.device)
